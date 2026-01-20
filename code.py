de75e8aa4631363cf94d11340429059a86004d44
unknown <jefferytian628@gmail.com>
2026-01-20 13:29:47 -0500

    New circuitpython code with updated GPIO pins

--- code.py
+++ code.py
@@ -1,52 +1,63 @@
-from machine import Pin, PWM
-from utime import sleep
+'''
+ESC204 2023S Lab 2 Task D
+Task: Light up external LED on button press.
+'''
+# Import libraries needed for blinking the LED
+import board
+import digitalio
+import asyncio
+import countio
+import time
+import alarm
+
+# Configure the internal GPIO connected to the LED as a digital output
+ledb = digitalio.DigitalInOut(board.GP7)
+ledb.direction = digitalio.Direction.OUTPUT
+
+ledg = digitalio.DigitalInOut(board.GP11)
+ledg.direction = digitalio.Direction.OUTPUT
+
+ledr = digitalio.DigitalInOut(board.GP6)
+ledr.direction = digitalio.Direction.OUTPUT
+
+# Configure the internal GPIO connected to the button as a digital input
+button = digitalio.DigitalInOut(board.GP15)
+button.direction = digitalio.Direction.INPUT
+button.pull = digitalio.Pull.DOWN # Set the internal resistor to pull-up
 
 def mode_1():
-    pinO1.value(1)
-    pinO2.value(1)
-    pinO3.value(0)
+	ledr.value = True
+	ledg.value = True
+	ledb.value = False
 
 def mode_2():
-    pinO1.value(1)
-    pinO2.value(1)
-    pinO3.value(1)
+	ledr.value = True
+	ledg.value = True
+	ledb.value = True
 
 def mode_3():
-    pinO1.value(0)
-    pinO2.value(0)
-    pinO3.value(0)
+	ledr.value = False
+	ledg.value = False
+	ledb.value = False
 
-led1 = 1
-led2 = 2
-led3 = 4
-in1 = 17
-#in2 = 23
-in3 = 24
+count = 0
 
-pinO1 = Pin(led1, Pin.OUT)
-pinO2 = Pin(led2, Pin.OUT)
-pinO3 = Pin(led3, Pin.OUT)
+async def button_pressed(pin):
+	with countio.Counter(pin) as interrupt:
+		while True:
+			if interrupt.count > 1:
+				interrupt.count = 0
+				count += 1
+				if(count % 3 == 1):
+					mode_1()
+				elif(count % 3 == 2):
+					mode_2()
+				else:
+					mode_3()
+			await asyncio.sleep(0)
 
-pinI = Pin(in1, Pin.IN, Pin.PULL_DOWN)
-#pin5 = Pin(in2, Pin.IN, Pin.PULL_DOWN)
-#pin6 = Pin(in3, Pin.IN, Pin.PULL_DOWN)
+async def main():
+	button_wait = asyncio.create_task(button_pressed(button))
+	await asyncio.gather(button_wait)
 
-def mode_change(Pin):
-    pass
-
-#gonna need to check PWM 
-if __name__ == "__main__":
-    print("LED starts flashing...")
-    pinI.irq(trigger = Pin.IRQ_RISING, handler = mode_change)
-    while True:
-        try:
-            mode_1()
-            sleep(1) # sleep 1sec
-            mode_2()
-            sleep(1)
-            mode_3()
-            sleep(1)
-        except KeyboardInterrupt:
-            break
-    pinO1.off()
-    print("Finished.")
+asyncio.run(main)
