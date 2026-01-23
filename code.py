e99ad748a2278c9f1e3f84668dbf9defa1491fb0
unknown <jefferytian628@gmail.com>
2026-01-22 21:30:55 -0500

    Fixed issue where the controller would return to mode 2 after the extended button press instead of mode 1

--- code.py
+++ code.py
@@ -12,14 +12,14 @@ import analogio
 
 # Configure the internal GPIO connected to the LED as a digital output
 
-ledr = digitalio.DigitalInOut(board.GP6)
-ledr.direction = digitalio.Direction.OUTPUT
+#ledr = digitalio.DigitalInOut(board.GP6)
+#ledr.direction = digitalio.Direction.OUTPUT
 
-ledg = digitalio.DigitalInOut(board.GP11)
-ledg.direction = digitalio.Direction.OUTPUT
+#ledg = digitalio.DigitalInOut(board.GP11)
+#ledg.direction = digitalio.Direction.OUTPUT
 
-ledb = digitalio.DigitalInOut(board.GP7)
-ledb.direction = digitalio.Direction.OUTPUT
+#ledb = digitalio.DigitalInOut(board.GP7)
+#ledb.direction = digitalio.Direction.OUTPUT
 
 # Configure the internal GPIO connected to the button as a digital input
 button = digitalio.DigitalInOut(board.GP15)
@@ -30,53 +30,86 @@ pwmr = pwmio.PWMOut(board.GP6, frequency=5000, duty_cycle=0)
 pwmg = pwmio.PWMOut(board.GP11, frequency=5000, duty_cycle=0)
 pwmb = pwmio.PWMOut(board.GP7, frequency=5000, duty_cycle=0)
 
-knob = analogio.AnalogIn(board.ADC0)
+
 
 def mode_1():
-	ledr.value = False
-	ledg.value = False
-	ledb.value = False
+	pwmr.duty_cycle = 0
+	pwmg.duty_cycle = 0
+	pwmb.duty_cycle = 0
+	mode = 1
+	return mode
 
-def mode_2():
-	ledr.value = True
-	ledg.value = True
-	ledb.value = True
+def mode_2(raw):
+	pwmr.duty_cycle = raw
+	pwmg.duty_cycle = raw
+	pwmb.duty_cycle = raw
+	print(pwmr.duty_cycle)
+	print(pwmg.duty_cycle)
+	mode = 2
+	return mode
 
-def mode_3(edge_count, time_start, cur_time):
-	if((time_start != 0) and (cur_time - time_start) > 10):
+def mode_3(raw, edge_count, time_start, cur_time, mode):
+	if((time_start != 0) and (cur_time - time_start) > 5):
 		time_start = 0
 		edge_count = 1
-		mode_1()
+		mode = mode_1()
 	else:
-		ledr.value = True
-		ledg.value = True
-		ledb.value = False
-	return edge_count
+		pwmr.duty_cycle = raw
+		pwmg.duty_cycle = 0
+		pwmb.duty_cycle = raw
+		print(pwmr.duty_cycle)
+		print(pwmg.duty_cycle)
+		mode = 3
+	return edge_count, mode
 
-def change_led_brightness(knob):
-	raw = knob.value
-	print(raw)
+def change_led_brightness(raw, delay_start, knob, mode):
+	if(delay_start == 0):
+		print("ok")
+		delay_start = time.monotonic()
+	elif(time.monotonic() - delay_start > 0.01):
+		print("check")
+		raw = knob.value
+		delay_start = 0
+	if(mode == 1):
+		pwmr.duty_cycle = 0
+		pwmg.duty_cycle = 0
+		pwmb.duty_cycle = 0
+	elif(mode == 2):
+		pwmr.duty_cycle = raw
+		pwmg.duty_cycle = raw
+		pwmb.duty_cycle = raw
+	else:
+		pwmr.duty_cycle = raw
+		pwmg.duty_cycle = 0
+		pwmb.duty_cycle = raw
+	return raw, delay_start
 
 # Loop so the code runs continuously
 if __name__ == "__main__":
-	edge_count = 0
+	edge_count = 1
 	time_start = 0
 	cur_time = 0
+	delay_start = 0
+	knob = analogio.AnalogIn(board.GP26)
+	raw = 60000
+	mode = 1
 	while True:
 		while(button.value):
-			change_led_brightness(knob)
+			raw, delay_start = change_led_brightness(raw, delay_start, knob, mode)
 		while(not button.value):
-			change_led_brightness(knob)
+			raw, delay_start = change_led_brightness(raw, delay_start, knob, mode)
 		edge_count += 1
 		cur_time = time.monotonic()
-		print()
 		if((edge_count % 3) == 1):
 			time_start = 0
-			mode_1()
+			mode = mode_1()
 		elif((edge_count % 3) == 2):
-			mode_2()
+			mode = mode_2(raw)
 			time_start = time.monotonic()
 		else:
-			edge_count = mode_3(edge_count, time_start, cur_time)
-		time.sleep(0.1)
+			edge_count, mode = mode_3(raw, edge_count, time_start, cur_time, mode)
+		time.sleep(0.5)
+
+
+
 
